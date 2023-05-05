b45c2988856ba4016866d4d31aeed62a78a2cf9c
Janhavi2916 <128283083+Janhavi2916@users.noreply.github.com>
2023-05-05 09:12:58 +0530

    Add files via upload

---
+++ Pattern.java
@@ -0,0 +1,17 @@
+public class SolidRectanglepattern 
+{
+	public static void main(String[] args) 
+	{
+		int n=4;
+		int m=5;
+		//outer loop row
+		for (int i = 1; i <= n; i++) 
+		{
+			//inner loop column
+			for (int j=1; j<=m; j++) 
+			{
+				System.out.print("1");
+			}System.out.println();
+		}
+	}
+}
